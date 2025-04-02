e9bd749c35988c9b9dc1bfb6e0c88d7e60aef917
honorine <honorinesawadogo07@gmail.com>
2025-04-02 14:03:18 +0000

    Mise à jour de App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,23 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import ListeScreen from "./Screens/ListeScreen"; 
-import LoginScreen from "./Screens/LoginScreen"; 
-import RegisterScreen from "./Screens/RegisterScreen"; 
-import ProfileScreen from "./Screens/ProfileScreen"; 
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "./Screens/LoginScreen";
+import RegisterScreen from "./Screens/RegisterScreen";
+import ListScreen from "./Screens/ListScreen";
+import ProfileScreen from "./Screens/ProfileScreen";
 
-const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
-const App = () => {
+export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Login" component={LoginScreen} />
-        <Tab.Screen name="Register" component={RegisterScreen} />
-        <Tab.Screen name="Liste" component={ListeScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{ title: "Connexion" }} />
+        <Stack.Screen name="Register" component={RegisterScreen} options={{ title: "Inscription" }} />
+        <Stack.Screen name="List" component={ListScreen} options={{ title: "Liste des Utilisateurs" }} />
+        <Stack.Screen name="Profile" component={ProfileScreen} options={{ title: "Profil Utilisateur" }} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
-};
+}
 
-export default App;
